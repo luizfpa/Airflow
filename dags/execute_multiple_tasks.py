b15dbfbc19cd29aa7df59cad721c3fa31b6a8ee4
Luiz Fernando <109439503+lfparaujo@users.noreply.github.com>
2025-07-11 14:37:44 -0700

    multiple tasks in down and upstream

--- dags/execute_multiple_tasks.py
+++ dags/execute_multiple_tasks.py
@@ -12,19 +12,50 @@ with DAG(
     dag_id = 'executing_multiple_tasks',
     description = 'DAG with multiple tasks and dependencies',
     default_args = default_args,
-    tags = ['Lrn Airflow', 'LinkedIn'],
+    tags = ['Lrn Airflow', 'LinkedIn','down/up'],
     start_date = airflow.utils.dates.days_ago(1),
-    schedule_interval = '@once'
+    schedule_interval = timedelta(days=1)
 ) as dag:
     
     taskA = BashOperator(
         task_id = 'taskA',
-        bash_command = 'echo TASK A has executed.'
+        bash_command = '''
+        echo TASK A has started!
+
+        for i in {1..10}
+        do
+            echo TASK A printing $i
+        done
+
+        echo TASK A has ended!
+        '''
     )
 
     taskB = BashOperator(
         task_id = 'taskB',
-        bash_command = 'echo Task B has executed.'
+        bash_command = '''
+        echo TASK B has started!
+        sleep 4
+        echo TASK B has ended!
+        '''
+    )
+
+    taskC = BashOperator(
+        task_id = 'taskC',
+        bash_command = '''
+        echo TASK C has started!
+        sleep 15
+        echo TASK C has ended!
+        '''
+    )
+
+    taskD = BashOperator(
+        task_id = 'taskD',
+        bash_command = 'echo TASK D completed!'
     )
 
 taskA.set_downstream(taskB)
+taskA.set_downstream(taskC)
+
+taskD.set_upstream(taskB)
+taskD.set_upstream(taskC)
