9631b8ada9c247d3da9514dcabba2197155a8b8a
Luiz Fernando <109439503+lfparaujo@users.noreply.github.com>
2025-07-11 15:33:12 -0700

    adding template search and script files

--- dags/execute_multiple_tasks.py
+++ dags/execute_multiple_tasks.py
@@ -12,47 +12,47 @@ with DAG(
     dag_id = 'executing_multiple_tasks',
     description = 'DAG with multiple tasks and dependencies',
     default_args = default_args,
-    tags = ['Lrn Airflow', 'LinkedIn','down/up'],
+    tags = ['Lrn Airflow', 'LinkedIn','template_search'],
     start_date = airflow.utils.dates.days_ago(1),
-    schedule_interval = timedelta(days=1)
+    schedule_interval = timedelta(days=1),
+    template_searchpath = '/opt/airflow/dags/bash_scripts'
 ) as dag:
     
     taskA = BashOperator(
         task_id = 'taskA',
-        bash_command = '''
-        echo TASK A has started!
-
-        for i in {1..10}
-        do
-            echo TASK A printing $i
-        done
-
-        echo TASK A has ended!
-        '''
+        bash_command = 'taskA.sh'
     )
 
     taskB = BashOperator(
         task_id = 'taskB',
-        bash_command = '''
-        echo TASK B has started!
-        sleep 4
-        echo TASK B has ended!
-        '''
+        bash_command = 'taskB.sh'
     )
 
     taskC = BashOperator(
         task_id = 'taskC',
-        bash_command = '''
-        echo TASK C has started!
-        sleep 15
-        echo TASK C has ended!
-        '''
+        bash_command = 'taskC.sh'
     )
 
     taskD = BashOperator(
         task_id = 'taskD',
-        bash_command = 'echo TASK D completed!'
+        bash_command = 'taskD.sh'
     )
 
-taskA >> [taskB, taskC]
-taskD << [taskB, taskC]
+    taskE = BashOperator(
+        task_id = 'taskE',
+        bash_command = 'taskE.sh'
+    )
+
+    taskF = BashOperator(
+        task_id = 'taskF',
+        bash_command = 'taskF.sh'
+    )
+
+    taskG = BashOperator(
+        task_id = 'taskG',
+        bash_command = 'taskG.sh'
+    )
+
+taskA >> taskB >> taskE
+taskA >> taskC >> taskF
+taskA >> taskD >> taskG
