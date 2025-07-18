06d3bc40c59e1d256cbf3ad9c9c0d486a9d5f76f
Luiz Fernando <109439503+lfparaujo@users.noreply.github.com>
2025-07-18 15:22:10 -0700

    changes made on DockerFile and docker-compose because of the output file

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,12 @@
 FROM apache/airflow:2.10.2
 USER airflow
 
-RUN pip install --no-cache-dir pandas==2.2.2 pyspark==3.5.0 psycopg2-binary==2.9.9
+#RUN apt-get update && apt-get install -y wget && rm -rf /var/lib/apt/lists/*
 
-# Configurar JAVA_HOME
-ENV JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
-
-# Criar diretório para JARs e baixar PostgreSQL JDBC
-RUN mkdir -p /opt/airflow/jars && \
-    wget -O /opt/airflow/jars/postgresql-42.7.7.jar \
-    https://jdbc.postgresql.org/download/postgresql-42.7.7.jar
-
+USER airflow
+RUN pip install --no-cache-dir pandas==2.2.2 psycopg2-binary==2.9.9
+#RUN mkdir -p /opt/airflow/jars && \
+#    wget -O /opt/airflow/jars/postgresql-42.7.7.jar \
+#    https://jdbc.postgresql.org/download/postgresql-42.7.7.jar
 
 USER ${AIRFLOW_UID:-50000}
--- dags/executing_python_pipeline.py
+++ dags/executing_python_pipeline.py
@@ -15,13 +15,45 @@ def read_csv_file():
     print(df)
     return df.to_json()
 
+def remove_null_values(**kwargs):
+    ti = kwargs['ti']
+    json_data = ti.xcom_pull(task_ids='read_csv_file')
+    df = pd.read_json(json_data)
+    df = df.dropna()
+    print(df)
+    return df.to_json()
+
+def groupby_smoker(ti):
+    json_data = ti.xcom_pull(task_ids='remove_null_values')
+    df = pd.read_json(json_data)
+    smoker_df = df.groupby('smoker').agg({
+        'age':'mean',
+        'bmi':'mean',
+        'charges': 'mean'
+    }).reset_index()
+    smoker_df.to_csv(
+        '/opt/airflow/output/grouped_by_smoker.csv', index=False
+    )
+
+def groupby_region(ti):
+    json_data = ti.xcom_pull(task_ids='remove_null_values')
+    df = pd.read_json(json_data)
+    region_df = df.groupby('region').agg({
+        'age':'mean',
+        'bmi':'mean',
+        'charges':'mean'
+    }).reset_index()
+    region_df.to_csv(
+        '/opt/airflow/output/grouped_by_region.csv', index=False
+    )
+
 with DAG(
     dag_id = 'python_pipeline',
     description = 'Running a Python Pipeline',
     default_args = default_args,
     start_date = airflow.utils.dates.days_ago(1),
     schedule_interval = '@once',
-    tags = ['LinkedIn', 'LrnAirflow', 'Python_Pipeline']
+    tags = ['LinkedIn', 'LrnAirflow', 'Python_Pipeline','transform']
 )as dag:
 
     read_csv_file = PythonOperator(
@@ -29,4 +61,19 @@ with DAG(
         python_callable = read_csv_file
     )
 
-read_csv_file
+    remove_null_values = PythonOperator(
+        task_id = 'remove_null_values',
+        python_callable = remove_null_values
+    )
+
+    groupby_smoker = PythonOperator(
+        task_id = 'groupby_smoker',
+        python_callable = groupby_smoker
+    )
+
+    groupby_region = PythonOperator(
+        task_id = 'groupby_region',
+        python_callable = groupby_region
+    )
+
+read_csv_file >> remove_null_values >> [groupby_smoker, groupby_region]
